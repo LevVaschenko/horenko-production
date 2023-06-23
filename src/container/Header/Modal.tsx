3488b26d0ec931664de39e4a06201b8fd4897af1
Lev <levvaschenko@gmail.com>
2023-06-23 00:24:06 -0700

    add input mask for phone form and submit form

--- src/container/Header/Modal.tsx
+++ src/container/Header/Modal.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Modal from '@mui/material/Modal'
 import './Modal.scss'
+import Check from 'assets/Check.png'
+import InputMask from 'react-input-mask'
 
 type ModalProps = {
     open: boolean
@@ -8,53 +10,86 @@ type ModalProps = {
 }
 
 const CustomModal: React.FC<ModalProps> = ({ open, onClose }) => {
-    const handlePhoneInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const enteredValue = event.target.value;
+    const [nameInputValue, setNameInputValue] = useState('')
+    const [phoneInputValue, setPhoneInputValue] = useState('')
+    const [isNameInputEmpty, setIsNameInputEmpty] = useState(false)
+    const [isPhoneInputEmpty, setIsPhoneInputEmpty] = useState(false)
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false)
 
-        if (enteredValue.startsWith('+380')) {
-            event.target.classList.remove('phone-uninput');
+    const handleFormSubmit = () => {
+        const phoneRegex = /^\+380-\d{2}-\d{3}-\d{2}-\d{2}$/
+
+        if (nameInputValue === '' || phoneInputValue === '' || !phoneRegex.test(phoneInputValue)) {
+            setIsNameInputEmpty(nameInputValue === '')
+            setIsPhoneInputEmpty(phoneInputValue === '' || !phoneRegex.test(phoneInputValue))
         } else {
-            event.target.classList.add('phone-uninput');
+            // Additional form submission logic...
+            setIsFormSubmitted(true)
         }
-    };
+    }
+
+    const handlePhoneInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPhoneInputValue(event.target.value)
+        setIsPhoneInputEmpty(false)
+    }
+
+    const handleNameInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNameInputValue(event.target.value)
+    }
 
     return (
         <Modal open={open} onClose={onClose} className="custom-modal">
             <div className="modal-container">
                 <div className="modal-close" onClick={onClose} />
                 <div className="modal-text">
-                    <div className="modal-title">
-                        Залишайте заявку
-                    </div>
-                    <div className="modal-description">
-                        Ми зв’яжемося з вами найближчим часом.
-                    </div>
-                    <div className="modal-forms">
-                        <div className="input-container">
-                            <input type="text" name="name" placeholder="Ваше ім’я*" />
+                    {isFormSubmitted ? (
+                        <div className="form-submitted">
+                            <div className="check-container">
+                                <img src={Check} alt="" className="check" />
+                            </div>
+                            <div className="modal-title">Дякуємо за заявку!</div>
+                            <div className="modal-description">Ваші дані успішно надіслані.</div>
                         </div>
-                        <div className="input-container">
-                            <input
-                                type="tel"
-                                name="phone"
-                                placeholder="+380-00-000-00-00*"
-                                className='phone-input'
-                                onChange={handlePhoneInputChange}
-                                list="country-codes"
-                            />
-                            <datalist id="country-codes">
-                                <option value="+380">Ukraine</option>
-                            </datalist>
+                    ) : (
+                        <div>
+                            <div className="modal-title">Залишайте заявку</div>
+                            <div className="modal-description">Ми зв’яжемося з вами найближчим часом.</div>
+                            <div className="modal-forms">
+                                <div className={`input-container ${isNameInputEmpty ? 'empty-input' : ''}`}>
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        placeholder="Ваше ім’я*"
+                                        onChange={handleNameInputChange}
+                                        value={nameInputValue}
+                                    />
+                                </div>
+                                <div className={`input-container ${isPhoneInputEmpty ? 'empty-input' : ''}`}>
+                                    <InputMask
+                                        mask="+380-99-999-99-99"
+                                        maskChar=" "
+                                        type="tel"
+                                        name="phone"
+                                        placeholder="+380-00-000-00-00*"
+                                        className={`phone-input ${phoneInputValue.startsWith('+380') ? '' : 'phone-uninput'}`}
+                                        onChange={handlePhoneInputChange}
+                                        value={phoneInputValue}
+                                        list="country-codes"
+                                    />
+                                    <datalist id="country-codes">
+                                        <option value="+380">Ukraine</option>
+                                    </datalist>
+                                </div>
+                            </div>
+                            <div className="modal-send" onClick={handleFormSubmit}>
+                                Відправить
+                            </div>
+                            <div className="modal-processing">
+                                Натискаючи кнопку “Надіслати”, ви погоджуєтесь з
+                                <span className="rules-processing"> Правилами обробки персональних даних.</span>
+                            </div>
                         </div>
-                    </div>
-                    <div className="modal-send">
-                        Відправить
-                    </div>
-                    <div className="modal-processing">
-                        Натискаючи кнопку “Надіслати”, ви погоджуєтесь з
-                        <span className='rules-processing'> Правилами обробки персональних даних.
-                        </span>
-                    </div>
+                    )}
                 </div>
             </div>
         </Modal>
